79fbe98513617effbbe3014cf4fa8d72fc26712a
Ronnie Gonzales <ronnie_cepafs@hotmail.com>
2025-01-24 16:17:18 -0500

    Segundo commit

---
+++ src/main/java/com/pruebabcp/evaluacion/evaluacionmicroserviciosreativos/EvaluacionmicroserviciosreativosApplication.java
@@ -0,0 +1,13 @@
+package com.pruebabcp.evaluacion.evaluacionmicroserviciosreativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionmicroserviciosreativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionmicroserviciosreativosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebabcp/evaluacion/evaluacionmicroserviciosreativos/EvaluacionmicroserviciosreativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebabcp.evaluacion.evaluacionmicroserviciosreativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionmicroserviciosreativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
